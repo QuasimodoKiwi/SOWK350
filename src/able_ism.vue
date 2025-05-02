851be6ce24ae06e22260163e944d002f7e8f14c7
Connor Hamilton <clyhamilton@gmail.com>
2025-05-02 16:29:56 -0400

    first commit

---
+++ src/able_ism.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="article-wrapper">
+    <div class="bg-img"></div>
+    <main>
+      <div class="article-title">
+        <h1>Ableism</h1>
+        <h3>By Connor Hamilton</h3>
+        <h3>5/1/25</h3>
+      </div>
+      <section class="section1">
+        <h1>My Initial Thoughts</h1>
+        <p>
+          Initially, I was looking for classes to pick as a perspectives course for my Spring 2025 semester,
+          as suggested by my advisor Dr. Hardy. I can't remember all the perspectives courses on the
+          Course Registration portal, but I settled for the Human Diversity course as I thought it would look
+          good on my resume, and that I also think there were aspects of my beliefs that could be refined.
+        </p>
+      </section>
+
+      <section class="section2">
+        <h1>Revelation</h1>
+        <p>
+          Autism plays a major part of my life, that makes up who I am.
+          I was diagnosed when I was around 4-5 years old, on the lower-end of the spectrum according to my mother.
+          As a kid, I found it hard to make friends, and had a few good friends who I still keep in contact with.
+        </p>
+        <p>
+          I never knew I had autism until my mother finally told me when I was 13-14 years old.
+          I knew I was different from other people back then, but the news helped me to see myself in
+          a different light a few years after the reveal. I never fully appreciated my autism until I
+          met one of my best friends at the end of middle school who I didn't know was autistic, but turned my life around
+          after meeting them.
+        </p>
+        <p>
+          It wasn't until I was in ninth grade that the term "sped(s)" was being spread around.
+          It is a derogatory term, used by the students back then to call someone slow, stupid, and moreso
+          how intellectually low their perception of the receiver is. To hear that coming from someone hurts.
+          But the thing is, they don't realize the potential that is dormant within a disabled person.
+        </p>
+      </section>
+
+      <section class="section3">
+        <h1>Ableism</h1>
+        <p>
+          To me, ableism is the type of discrimination, oppression, or bias towards people
+          for their disabilities and/or impairments that affect their everyday life.
+        </p>
+        <p>
+          It is a bias that goes beyond centuries of disabled people being compared to the likes of the non-disabled,
+          by the privileged dominant groups in society that lack the context and failed empathy towards the people who
+          were born in a certain way, or their difficulties to keep up with the non-disabled.
+        </p>
+        <p>
+          I've met and worked with people who have disabilities, mothers and fathers too.
+          They're people who aren't vocal about their struggles, but they're doing their best to survive inside.
+        </p>
+      </section>
+
+      <section class="section4">
+        <h1>Treating those with Physical Disabilities and Mental Disabilities with Respect</h1>
+        <p>
+          When I talk about people with physical disabilities, I talk about those everyday people in our lives
+          who are handicapped...
+        </p>
+        <ul>
+          <li>...who find difficulty in moving...</li>
+          <li>...find difficulty in using their entire hand...</li>
+          <li>...humans that can't see straight or at all...</li>
+          <li>...and those who are handicapped in using limbs or body parts for everyday life.</li>
+        </ul>
+        <p>
+          For the people with mental disabilities, they are also people living their everyday lives with inner burdens, disabilities including,
+        </p>
+        <ul>
+          <li>Autism Spectrum Disorder - characterized by repetitive and inflexible patterns of behavior and interests.</li>
+          <li>Bipolar disorder - characterized by periods of depression and elevated moods, that can last several days, weeks, months, etc.</li>
+          <li>Anxiety disorders - characterized by uncontrollable feelings of anxiety and fear that can impair someone's social, occupational, and personal life.</li>
+          <li>Intellectual disorder - characterized by impairments in intellectual and adaptive functioning.</li>
+        </ul>
+        <p>
+          I don't know why non-disabled people treat those with mental disabilities differently than those with physical difficulties.
+          Society has a way of treating those with physical disabilities with respect, but for those who carry different disabilities,
+          they are seen as different. They are ostracized from job opportunities, relationship opportunities, and more.
+          </p>
+      </section>
+      <section class="section5">
+        <h1>The significance of Ableism to me</h1>
+        <p>
+          As a person on the spectrum, I don't expect everyone to be kind to me, or fully comprehend the social difficulties that I go through.
+          After all, we are an imperfect human species, capable of kindness, empathy, kinship, and much more.
+          Yet, I find that we all still have growing to do as a species,
+          but society can progress if you pay more attention to your fellow neighbor living their life,
+          and even attempt to befriend them. A disadvantaged person like me would have nothing more than
+          to be able to live comfortably among my peers and live on the same level as them.
+        </p>
+        <p>I may have been oppressed</p>
+      </section>
+      <section class="section6">
+        <h1>The Aftermath of SOWK350</h1>
+        <p>
+          As a result of taking this class, the beliefs and assumptions I had going into this class were refined
+          when I opened the book and spent countless nights and Sundays reading chapters about human diversity.
+          I am aware of some topics that were already apparent with my generation, but there were few topics that
+          I admit I never heard of, like Ageism.
+        </p>
+        <p>
+          This past semester I was able to talk and meet with an amazing group, like Alana, Chloe, Leah, and Broderick.
+          The hours we spent talking about current-day subjects was like a breath of fresh air to me. I never got to talk
+          about these issues that are going on in our society, and the meetings all felt natural because we were having adult
+          conversations in the real world. The knowledge we shared with each other about our society felt grounded and nautral,
+          and everyone was surprisingly competent with each other.
+        </p>
+
+        <p>
+          Back in March, I went through an internal crisis that made me want to quit Computer Science completely. I was failing,
+          and the six years of knowledge I gained from my sophomore year of high school to my junior year of college
+          would have all been for nothing.
+        </p>
+        <p>
+          I had a meeting with my advisor who told me that even though I don't want to do Computer Science
+          anymore, I would much rather have it as a hobby. This creative project gave me an opportunity to attempt to put the skills I gained
+          from Thaddeus Stevens to use. In a way, the skills I learned in the six years of programming will never fade away,
+          and neither will the stories of the oppressed from our readings will remain with me too and hold dear as I try to unify with more people in the future.
+        </p>
+      </section>
+
+  </main>
+</div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import "./assets/ableism.css";
+
+import site from './able_ism.vue'
+
+createApp(site).mount('#app');
